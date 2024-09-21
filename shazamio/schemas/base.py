3e8899a37308336ff92cbbed7173802b455f8149
kinkyninja <roman.jarenkov@gmail.com>
2024-09-21 18:31:19 +0300

    Update base.py

--- shazamio/schemas/base.py
+++ shazamio/schemas/base.py
@@ -10,18 +10,18 @@ class BaseIdTypeHref(BaseHref):
     id: str
     type: str
 
-class BaseIdTypeHrefAttributesModel(BaseIdTypeHref):
+class BaseIdTypeHrefAttributesModel(BaseIdTypeHref, Generic[T]):
     attributes: T
 
-class BaseAttributesModel(BaseModel):
+class BaseAttributesModel(BaseModel, Generic[T]):
     attributes: T
 
-class BaseDataModel(BaseModel):
+class BaseDataModel(BaseModel, Generic[T]):
     data: T
 
-class BaseHrefNextData(BaseHref):
+class BaseHrefNextData(BaseHref, Generic[T]):
     next: Optional[str] = None
     data: T
 
-class BaseHrefData(BaseHref):
+class BaseHrefData(BaseHref, Generic[T]):
     data: T
