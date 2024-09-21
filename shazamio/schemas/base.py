891f309a43d740be0e0f199f39c57df0ceebe98c
kinkyninja <roman.jarenkov@gmail.com>
2024-09-21 18:18:00 +0300

    Update base.py

--- shazamio/schemas/base.py
+++ shazamio/schemas/base.py
@@ -1,38 +1,27 @@
-from typing import Generic
-from typing import Optional
-from typing import TypeVar
-
+from typing import Generic, TypeVar, Optional
 from pydantic import BaseModel
-from pydantic.generics import GenericModel
 
-T = TypeVar("T", bound=BaseModel)
-
+T = TypeVar("T")
 
 class BaseHref(BaseModel):
     href: str
-
 
 class BaseIdTypeHref(BaseHref):
     id: str
     type: str
 
-
-class BaseIdTypeHrefAttributesModel(GenericModel, BaseIdTypeHref, Generic[T]):
+class BaseIdTypeHrefAttributesModel(BaseModel, BaseIdTypeHref, Generic[T]):
     attributes: T
 
-
-class BaseAttributesModel(GenericModel, BaseModel, Generic[T]):
+class BaseAttributesModel(BaseModel, Generic[T]):
     attributes: T
 
-
-class BaseDataModel(GenericModel, BaseModel, Generic[T]):
+class BaseDataModel(BaseModel, Generic[T]):
     data: T
 
-
-class BaseHrefNextData(GenericModel, BaseHref, Generic[T]):
+class BaseHrefNextData(BaseModel, BaseHref, Generic[T]):
     next: Optional[str] = None
     data: T
 
-
-class BaseHrefData(GenericModel, BaseHref, Generic[T]):
+class BaseHrefData(BaseModel, BaseHref, Generic[T]):
     data: T
